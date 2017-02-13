5bdb3b5c158e15cb41e78f9e4a55f3682b7b3561
Matthieu BRAULT <matthieu.brault@epitech.eu>
2017-02-13 13:02:28 +0100

    c'est presque beau

--- calculate.c
+++ calculate.c
@@ -5,7 +5,7 @@
 ** Login   <b00bix@epitech.net>
 ** 
 ** Started on  Thu Feb  9 14:51:38 2017 Matthieu BRAULT
-** Last update Fri Feb 10 16:41:00 2017 Matthieu BRAULT
+** Last update Mon Feb 13 13:02:18 2017 Matthieu BRAULT
 */
 
 #include <SFML/Graphics.h>
@@ -58,24 +58,33 @@ void	my_calculate(char *str, t_my_framebuffer *buffer)
   height = 0;
   screen_size.x = buffer->width;
   screen_size.y = buffer->height;
+  eye_pos.x = -200;
+  eye_pos.y = 0;
+  eye_pos.z = 0;
   while (height <= screen_size.y)
     {
       width = 0;
       while (width <= screen_size.x)
 	{
-	  eye_pos.x = width;
-	  eye_pos.y = height;
-	  eye_pos.z = 0;
+	  coord.x = 0;
+	  coord.y = width;
+	  coord.z = height;
 	  dir_vector = calc_dir_vector(screen_size, eye_pos);
-	  sphere = my_sphere(eye_pos, dir_vector, 67);
-	  cone = my_cone(eye_pos, dir_vector, 44);
-	  cylinder = my_cylinder(eye_pos, dir_vector, 32);
-	  if (my_aff(cylinder, sphere, cone) == 1)
-	    my_put_pixel(buffer, width, height, sfRed);	  
-	  else if (my_aff(cylinder, sphere, cone) == 2)
+	  sphere = my_sphere(coord, dir_vector, 67);
+	  cone = my_cone(coord, dir_vector, 89);
+	  cylinder = my_cylinder(coord, dir_vector, 150);
+	  if (cone > 0)
 	    my_put_pixel(buffer, width, height, sfGreen);
-	  else if (my_aff(cylinder, sphere, cone) == 3)
+	  if (cylinder > 0)
+	    my_put_pixel(buffer, width, height, sfRed);
+	  if (sphere > 0)
 	    my_put_pixel(buffer, width, height, sfYellow);
+	  /* if (my_aff(cylinder, sphere, cone) == 1) */
+	  /*   my_put_pixel(buffer, width, height, sfRed);	   */
+	  /* else if (my_aff(cylinder, sphere, cone) == 2) */
+	  /*   my_put_pixel(buffer, width, height, sfGreen); */
+	  /* else if (my_aff(cylinder, sphere, cone) == 3) */
+	  /*   my_put_pixel(buffer, width, height, sfYellow); */
 	  width = width + 1;
 	}
       height = height + 1;
