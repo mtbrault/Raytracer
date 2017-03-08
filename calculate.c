140e802dcd11cab9e645027f537074c99621c592
Matthieu <matthieu.brault@epitech.eu>
2017-03-08 18:59:21 +0100

    gestion de la lumière

--- calculate.c
+++ calculate.c
@@ -5,44 +5,12 @@
 ** Login   <b00bix@epitech.net>
 ** 
 ** Started on  Thu Feb  9 14:51:38 2017 Matthieu BRAULT
-** Last update Wed Mar  8 14:34:18 2017 Matthieu BRAULT
+** Last update Wed Mar  8 16:09:52 2017 Matthieu BRAULT
 */
 
 #include <SFML/Graphics.h>
 #include "struct.h"
 #include "raytracer1.h"
-
-int	my_aff(float cylinder, float sphere, float cone)
-{
-  if (cone < 0 && cylinder < 0 && sphere > 0)
-    return (1);
-  if (cone > 0 && cylinder < 0 && sphere < 0)
-    return (2);
-  if (cone < 0 && cylinder > 0 && sphere < 0)
-    return (3);
-  if (cone < 0 && cylinder > 0 && sphere > 0)
-    {
-      if (cylinder <= sphere)
-	return (3);
-      if (sphere <= cylinder)
-	return (1);
-    }
-  if (cone > 0 && cylinder < 0 && sphere > 0)
-    {
-      if (cone <= sphere)
-	return (2);
-      if (sphere <= cone)
-	return (1);
-    }
-  if (cone > 0 && cylinder > 0 && sphere < 0)
-    {
-      if (cone <= cylinder)
-	return (2);
-      else
-	return (3);
-    }
-  return (0);
-}
 
 void	my_calculate(t_my_framebuffer *buffer)
 {
@@ -50,7 +18,6 @@ void	my_calculate(t_my_framebuffer *buffer)
   sfVector2i	coord;
   sfVector2i	screen_size;
   sfVector3f	eye_pos;
-  sfVector3f	spheree;
   float		cone;
   float		cylinder;
   float		sphere;
@@ -62,8 +29,8 @@ void	my_calculate(t_my_framebuffer *buffer)
   eye_pos = eye_pos;
   screen_size.x = buffer->width;
   screen_size.y = buffer->height;
-  eye_pos.x = -200;
-  eye_pos.y = 0;
+  eye_pos.x = -500;
+  eye_pos.y = 100;
   eye_pos.z = 0;
   while (height <= screen_size.y)
     {
@@ -72,28 +39,13 @@ void	my_calculate(t_my_framebuffer *buffer)
 	{
 	  coord.x = width;
 	  coord.y = height;
-	  spheree.x = 500;
-	  spheree.y = 500;
-	  spheree.z = 500;
 	  dir_vector = calc_dir_vector(500, screen_size, coord);
-	  sphere = intersect_sphere(eye_pos, dir_vector, 42);
+	  sphere = intersect_sphere(eye_pos, dir_vector, 180);
 	  cylinder = intersect_cylinder(eye_pos, dir_vector, 150);
 	  plane = intersect_plane(eye_pos, dir_vector);
-	  cone = intersect_cone(eye_pos, dir_vector, 20);
 	  if (sphere > 0)
-	    my_put_pixel(buffer, width, height, sfGreen);
-	  if (plane > 0)
-	    my_put_pixel(buffer, width, height, sfBlue);
-	  /* if (cylinder > 0) */
-	  /*   my_put_pixel(buffer, width, height, sfRed); */
-	  /* if (sphere > 0) */
-	  /*   my_put_pixel(buffer, width, height, sfYellow); */
-	  /* if (my_aff(cylinder, sphere, cone) == 1) */
-	  /*   my_put_pixel(buffer, width, height, sfRed);	   */
-	  /* else if (my_aff(cylinder, sphere, cone) == 2) */
-	  /*   my_put_pixel(buffer, width, height, sfGreen); */
-	  /* else if (my_aff(cylinder, sphere, cone) == 3) */
-	  /*   my_put_pixel(buffer, width, height, sfYellow); */
+	    my_put_pixel(buffer, width, height, sfRed);
+	  cone = intersect_cone(eye_pos, dir_vector, 20);
 	  width = width + 1;
 	}
       height = height + 1;
--- src/light.c
+++ src/light.c
@@ -5,14 +5,22 @@
 ** Login   <b00bix@epitech.net>
 ** 
 ** Started on  Sun Feb 26 15:46:30 2017 Matthieu BRAULT
-** Last update Tue Feb 28 18:49:47 2017 Matthieu BRAULT
+** Last update Wed Mar  8 18:59:02 2017 Matthieu BRAULT
 */
 
+#include <math.h>
 #include "raytracer1.h"
 
 float	get_light_coef(sfVector3f light_vector, sfVector3f normal_vector)
 {
-  light_vector = light_vector;
-  normal_vector = normal_vector;
-  return (-1.0);
+  float	norm_light;
+  float	norm_vector;
+  float	vector_product;
+  float	cosinus;
+
+  norm_light = sqrt(pow(light_vector.x, 2) + pow(light_vector.y, 2) + pow(light_vector.z, 2));
+  norm_vector = sqrt(pow(normal_vector.x, 2) + pow(normal_vector.y, 2) + pow(normal_vector.z, 2));
+  vector_product = light_vector.x * normal_vector.x + light_vector.y * normal_vector.y + light_vector.z * normal_vector.z;
+  cosinus = vector_product / (norm_light * norm_vector);
+  return (acos(cosinus));
 }
