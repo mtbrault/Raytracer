b10510bbd6655bfbba4e31f5dc694e4ee7f88ceb
Matthieu <matthieu.brault@epitech.eu>
2017-03-16 12:52:50 +0100

    norme

--- src/light.c
+++ src/light.c
@@ -5,7 +5,7 @@
 ** Login   <b00bix@epitech.net>
 ** 
 ** Started on  Sun Feb 26 15:46:30 2017 Matthieu BRAULT
-** Last update Thu Mar 16 12:36:01 2017 Matthieu BRAULT
+** Last update Thu Mar 16 12:52:42 2017 Matthieu BRAULT
 */
 
 #include <math.h>
@@ -18,11 +18,19 @@ float	get_light_coef(sfVector3f light_vector, sfVector3f normal_vector)
   float	vector_product;
   float	cosinus;
 
-  norm_light = sqrt(pow(light_vector.x, 2) + pow(light_vector.y, 2) + pow(light_vector.z, 2));
-  norm_vector = sqrt(pow(normal_vector.x, 2) + pow(normal_vector.y, 2) + pow(normal_vector.z, 2));
-  if ((normal_light * norm_vector) == 0)
+  norm_light = sqrt(pow(light_vector.x, 2) +
+		    pow(light_vector.y, 2) + pow(light_vector.z, 2));
+  norm_vector = sqrt(pow(normal_vector.x, 2) +
+		     pow(normal_vector.y, 2) + pow(normal_vector.z, 2));
+  if ((norm_light * norm_vector) == 0)
     return (0);
-  vector_product = light_vector.x * normal_vector.x + light_vector.y * normal_vector.y + light_vector.z * normal_vector.z;
+  vector_product = light_vector.x * normal_vector.x +
+    light_vector.y * normal_vector.y + light_vector.z * normal_vector.z;
   cosinus = vector_product / (norm_light * norm_vector);
-  return (acos(cosinus));
+  if (acos(cosinus) > 1)
+    return (1);
+  else if (acos(cosinus) < 0)
+    return (0);
+  else
+    return (acos(cosinus));
 }
